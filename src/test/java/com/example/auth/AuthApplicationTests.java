a81b0070fca68c70bf424243669d91ee79081905
Fernanda Kipper <nanda.kipper@gmail.com>
2023-06-22 23:37:43 -0300

    feat: initial commit

---
+++ src/test/java/com/example/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
